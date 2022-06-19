4aa0af382eaeb052f0a3f0f9353af5bf1fee55d3
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-06-19 08:17:49 +0430

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+
+file = input("Enter the image file name which consists of a digit number:")
+
+model = tf.keras.models.load_model("handwritten.model")
+
+img = cv2.imread(file)
+img = cv2.resize(img, (28, 28))[:,:,0]
+img = np.invert(np.array([img]))
+prediction = model.predict(img)
+print(f"the digit number in the image may be a {np.argmax(prediction)}")
