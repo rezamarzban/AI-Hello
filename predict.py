2effcd49d682f89f0d16d12e4798649938a9ab8c
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-06-25 22:54:11 +0430

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,9 +10,9 @@ img = cv2.imread(file)
 img = cv2.resize(img, (28, 28))[:,:,0]
 img = np.invert(np.array([img]))
 for i in range(0, 28):
-print('')
-for j in range(0, 28):
-print('\x1b[38;2;%d;80;180m'%img[0,i,j]+'$'+'\x1b[0m', end='')
+	print('')
+	for j in range(0, 28):
+		print('\x1b[38;2;%d;80;180m'%img[0,i,j]+'$'+'\x1b[0m', end='')
 print('')
 prediction = model.predict(img)
 print(f"the digit number in the image may be a {np.argmax(prediction)}")
